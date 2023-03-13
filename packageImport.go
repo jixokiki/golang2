a1d4b939b89a820abe8580570c137e5f1798b16d
rizkimaulana <rrizki402@gmail.com>
2023-03-14 01:34:05 +0700

    penambahan modullll

--- packageImport.go
+++ packageImport.go
@@ -1,6 +1,6 @@
 package main
 
-import "example/SRC/PEMBELAJARAN_GOLANG/penjumlahan"
+import "example/SRC/PEMBELAJARAN_GOLANG/golang2/Root/penjumlahan"
 
 /*
 untuk bisa diakses import dari functionnya nama parameter harus berukuran besar
